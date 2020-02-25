a4e5fd5d454e004df041df5ced6ff36ecd25a61f
elaimaz <fariawesley4@gmail.com>
2020-02-25 00:32:16 -0300

    Added Games model and controller

---
+++ GameList/Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GameList.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Plataform { get; set; }
+        public string Developer { get; set; }
+        public string Publisher { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+
+    }
+}
